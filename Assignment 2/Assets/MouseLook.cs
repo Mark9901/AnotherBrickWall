b44472d791e08649ea3fac9b823e663adacaca5e
Christian Lundgren <60134133+LundgrenChristian@users.noreply.github.com>
2021-02-21 21:57:29 +0100

    PlayerMovement

---
+++ Assignment 2/Assets/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mousesensitivity = 200.0f;
+    private float xRotation = 0.0f;
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mousesensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mousesensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+
+        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0.0f, 0.0f);
+        transform.Rotate(Vector3.up, mouseX);
+
+    }
+}
---
+++ Assignment 2/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float movementspeed;
+    public float jumpforce;
+    public bool grounded = false;
+    public bool isjumping = false;
+
+  
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (grounded == true)
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            rb.velocity = transform.right * x + transform.forward * z;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded == true)
+        {
+            isjumping = true;
+            StartCoroutine(JumpingStartAndStop());
+        }
+
+        if (isjumping == true)
+        {
+            rb.AddForce(Vector3.up * jumpforce, ForceMode.Acceleration);
+        }
+
+
+
+
+    }
+
+    private IEnumerator JumpingStartAndStop()
+    {
+        yield return new WaitForSeconds(0.1f);
+        isjumping = false;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = false;
+        }
+    }
+}
