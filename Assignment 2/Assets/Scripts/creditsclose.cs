d8a9e8de6620617a96f8c99168ca650af600d6e8
Mark9901 <78760951+Mark9901@users.noreply.github.com>
2021-02-22 10:47:15 +0100

    Added buttons for play and exit

---
+++ Assignment 2/Assets/Scripts/creditsclose.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class creditsclose : MonoBehaviour
+{
+    public GameObject playdisable;
+    public GameObject creditimage;
+    public GameObject creditdisable;
+    public GameObject exitdisable;
+    public GameObject title;
+
+    public void Credits()
+    {
+        playdisable.SetActive(false);
+        creditimage.SetActive(true);
+        exitdisable.SetActive(false);
+        creditdisable.SetActive(false);
+        title.SetActive(false);
+        
+
+    }
+    public void Return()
+    {
+        playdisable.SetActive(true);
+        creditimage.SetActive(false);
+        exitdisable.SetActive(true);
+        creditdisable.SetActive(true);
+        title.SetActive(true);
+       
+    }
+}
+
