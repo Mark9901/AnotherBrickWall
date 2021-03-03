44295b00b51391e4f044641fd5a24bb66342825d
Christian Lundgren <60134133+LundgrenChristian@users.noreply.github.com>
2021-03-03 10:55:38 +0100

    Checkpoints and deadly objects

---
+++ Assignment 2/Assets/CollisionScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+    Vector3 respawnPosition = Vector3.zero;
+    public Transform player;
+
+    
+    void Start()
+    {
+        respawnPosition = player.position;
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        
+        if (hit.gameObject.tag == "Checkpoint")
+        {
+            respawnPosition = player.position;
+            Debug.Log("checkpoint");
+        }
+        else if (hit.gameObject.CompareTag("Deadly"))
+        {
+            player.position = respawnPosition;
+            Debug.Log("deadly");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Checkpoint")
+        {
+            respawnPosition = player.position;
+            Debug.Log("checkpoint");
+        }
+    }
+}
