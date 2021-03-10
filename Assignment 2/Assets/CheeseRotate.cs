cda622b6b435da6e716b552abb870fb26a17f3c0
Christian Lundgren <60134133+LundgrenChristian@users.noreply.github.com>
2021-03-10 11:15:37 +0100

    Cheese

---
+++ Assignment 2/Assets/CheeseRotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheeseRotate : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        gameObject.transform.Rotate(Vector3.up, 1);
+    }
+}
