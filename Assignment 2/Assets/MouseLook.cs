fa7c04c325f4805727f11016b7101ad4d80ffa68
artemiszheng <79093708+artemiszheng@users.noreply.github.com>
2021-03-15 14:56:08 +0100

    Revert "d"
    This reverts commit cc3e025980ba70ce747538f7d741bd6ab49c77be, reversing
    changes made to 004c0af07ab5e02220a5f6f5e469477f4af36e98.

--- Assignment 2/Assets/MouseLook.cs
+++ Assignment 2/Assets/MouseLook.cs
@@ -1,15 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Cinemachine;
+
 public class MouseLook : MonoBehaviour
 {
     public float mousesensitivity = 200.0f;
-    private float LookAtPosition = 0.0f;
-    private float yRotation = 0.0f;
-    public Transform lookAt;
-
-
+    private float xRotation = 0.0f;
+    
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -19,26 +16,13 @@ public class MouseLook : MonoBehaviour
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mousesensitivity * Time.deltaTime;
-        yRotation -= mouseX;
-        yRotation = Mathf.Clamp(yRotation, -90.0f, 90.0f);
-        transform.Rotate(Vector3.up, mouseX);
-
         float mouseY = Input.GetAxis("Mouse Y") * mousesensitivity * Time.deltaTime;
-        LookAtPosition -= mouseY;
-        LookAtPosition = Mathf.Clamp(LookAtPosition, 1.0f, 60.0f);
-        lookAt.transform.localRotation= Quaternion.Euler(LookAtPosition, 0, 0);
 
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
 
-
-
-
-
-
-        //if(Input.GetKeyDown(KeyCode.Escape))
-        //{
-        //    Cursor.lockstate = CursorLockMode.None;
-        //    Cursor.visible = true;
-        //}
+        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0.0f, 0.0f);
+        transform.Rotate(Vector3.up, mouseX);
 
     }
 }
