cd0f823976b0095a5dcfeaf0ec53497e59d2c432
Mark9901 <78760951+Mark9901@users.noreply.github.com>
2021-03-17 12:16:14 +0100

    revert

---
+++ Assignment 2/Assets/MainMusic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMusic : MonoBehaviour
+{
+    private float loopTimer = 200.0f;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        loopTimer += Time.deltaTime;
+
+        if (loopTimer >= 201)
+        {
+            SoundManager.PlaySound("MainMusic");
+            loopTimer = 0.0f;
+        }
+       
+    }
+}
